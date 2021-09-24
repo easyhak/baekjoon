e7b58c10123d7d13abe83076d17291096ecc6158
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2021-09-24 20:20:54 +0900

    14718.py

--- python/14718.py
+++ python/14718.py
@@ -1,6 +1,6 @@
-# 14718.py
+# 용감한 용사 진수 / 14718.py
 # 출처 University > 충남대학교 > 생각하는 프로그래밍 대회  C번
-# 알고리즘: 브루트포스 알고리즘, 값 / 좌표 압축
+# 알고리즘 분류: 브루트포스 알고리즘, 값 / 좌표 압축
 # pypy3로 제출, 시간복잡도가 너무 크다
 import sys
 input = sys.stdin.readline
@@ -8,8 +8,7 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 soldier = []
 for i in range(n):
-    q = list(map(int,input().split()))
-    soldier.append(q)
+    soldier.append(list(map(int,input().split())))
 ans = 10000000
 for i in range(n):
     for j in range(n):

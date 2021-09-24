cf786328d955e6fcdb5652f9493936bb26400432
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2021-09-24 20:17:11 +0900

    14719

---
+++ python/14718.py
@@ -0,0 +1,28 @@
+# 14718.py
+# 출처 University > 충남대학교 > 생각하는 프로그래밍 대회  C번
+# 알고리즘: 브루트포스 알고리즘, 값 / 좌표 압축
+# pypy3로 제출, 시간복잡도가 너무 크다
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+soldier = []
+for i in range(n):
+    q = list(map(int,input().split()))
+    soldier.append(q)
+ans = 10000000
+for i in range(n):
+    for j in range(n):
+        for w in range(n):
+            a = soldier[i][0]
+            b = soldier[j][1]
+            c = soldier[w][2]
+            count = 0
+            for m in range(n):
+                if (soldier[m][0]<=a) and (soldier[m][1]<=b) and (soldier[m][2]<=c):
+                    count += 1
+            if count >= k:
+                ans = min(ans,(a+b+c))
+print(ans)
+    
+

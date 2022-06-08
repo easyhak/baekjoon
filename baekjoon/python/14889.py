ee8d05d4aa3d7ee9a54d81db725eb2c5e343e3a0
jihak <jooin2000@naver.com>
2022-06-08 20:31:35 +0900

    스타트와링크

--- baekjoon/python/14889.py
+++ baekjoon/python/14889.py
@@ -10,11 +10,11 @@ players=[]
 for i in range(n):
     players.append(list(map(int,input().split())))
 ans=2000
-for x in comb:
-    start,link=0,0
-    for a,b in list(permutations(x,2)):
+for x in range(len(comb)//2): # 절반까지만 한다.
+    start,link=0,0 # start,link의 값들을 저장할 변수
+    for a,b in list(permutations(comb[x],2)): # permuatation을 이용해서 구해주도록 다 더해주도록한다.
         start+=players[a][b]
-    for a,b in list(permutations(list(set(arr)-set(x)),2)):
+    for a,b in list(permutations(list(set(arr)-set(comb[x])),2)): 
         link+=players[a][b]
-    ans=min(abs(start-link),ans)
+    ans=min(abs(start-link),ans) # 절댓값 붙여줘서 최솟값을 구할 것
 print(ans)
